7248dd55120a4858ca866d2492b5abca3c763284
Benjamin Catalfo <bcatalfo@gmail.com>
2025-02-22 15:34:36 -0500

    Added a form to sign up for the newsletter, a form action for it, and some JSX.

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,31 @@
 export default function Home() {
-  return <div> Third Space </div>;
+  async function addToMailList(formData: FormData) {
+    "use server";
+    console.log(
+      `${formData.get("emailAddress")} signed up for the newsletter!`
+    );
+  }
+  return (
+    <div className="flex flex-col items-center">
+      <div className="flex justify-center m-7 p-4">
+        <header className="font-bold text-3xl">Third Space</header>
+      </div>
+      <div className="flex flex-col justify-center items-center m-5 p-5 border-2 rounded-full ">
+        <h1>Sign up for our newsletter!</h1>
+        <form action={addToMailList} className="m-3 p-3">
+          <input
+            type="text"
+            name="emailAddress"
+            className="border text-lg"
+          ></input>
+          <button
+            type="submit"
+            className="bg-black text-white rounded-full p-2 mx-2"
+          >
+            Sign Up
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
