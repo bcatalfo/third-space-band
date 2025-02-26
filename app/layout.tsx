7d38bdf245725d9dd2e4ff52d7868d59808a1a29
Benjamin Catalfo <bcatalfo@gmail.com>
2025-02-25 20:04:22 -0500

    Moved some code from the Page to the RootLayout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,13 @@
 import type { Metadata } from "next";
-import { League_Gothic } from "next/font/google";
+import { League_Gothic, Black_Ops_One } from "next/font/google";
 import "./globals.css";
 
 const leagueGothic = League_Gothic({
+  subsets: ["latin"],
+});
+
+const blackOpsOne = Black_Ops_One({
+  weight: "400",
   subsets: ["latin"],
 });
 
@@ -19,7 +24,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${leagueGothic.className} antialiased`}>
-        {children}
+        <div className="flex flex-col items-center">
+          <div className="flex justify-center m-7 p-4">
+            <header className={`font-bold text-3xl ${blackOpsOne.className}`}>
+              Third Space
+            </header>
+          </div>
+          {children}
+        </div>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,8 @@
 "use client";
 
-import { Black_Ops_One } from "next/font/google";
 import { useActionState } from "react";
 import { addToMailList } from "./actions";
 import { MailListState } from "./interface";
-
-const blackOpsOne = Black_Ops_One({
-  weight: "400",
-  subsets: ["latin"],
-});
 
 export default function HomePage() {
   const [mailListState, formAction] = useActionState(
@@ -17,49 +11,42 @@ export default function HomePage() {
   );
 
   return (
-    <div className="flex flex-col items-center">
-      <div className="flex justify-center m-7 p-4">
-        <header className={`font-bold text-3xl ${blackOpsOne.className}`}>
-          Third Space
-        </header>
-      </div>
-      <div className="flex flex-col justify-center items-center m-5 p-5 border-2 rounded-full text-xl">
-        <h1 className="text-2xl">Sign up for our newsletter!</h1>
-        <form action={formAction} className="m-3 mb-1 p-3">
-          <input
-            type="text"
-            name="emailAddress"
-            className="border text-lg"
-          ></input>
-          <button
-            type="submit"
-            className="bg-black text-white rounded-full p-2 mx-2"
-          >
-            Sign Up
-          </button>
-        </form>
-        {mailListState == MailListState.AlreadySignedUp && (
-          <>
-            <p className="m-2 mt-1 p-2 text-red-400">
-              You already signed up with that email address
-            </p>
-          </>
-        )}
-        {mailListState == MailListState.SucessfullySignedUp && (
-          <>
-            <p className="m-2 mt-1 p-2">
-              You have successfully signed up to our mailing list.
-            </p>
-          </>
-        )}
-        {mailListState == MailListState.ErrorSigningUp && (
-          <>
-            <p className="m-2 mt-1 p-2 text-red-400">
-              An error occured an you were not signed up for the mailing list.
-            </p>
-          </>
-        )}
-      </div>
+    <div className="flex flex-col justify-center items-center m-5 p-5 border-2 rounded-full text-xl">
+      <h1 className="text-2xl">Sign up for our newsletter!</h1>
+      <form action={formAction} className="m-3 mb-1 p-3">
+        <input
+          type="text"
+          name="emailAddress"
+          className="border text-lg"
+        ></input>
+        <button
+          type="submit"
+          className="bg-black text-white rounded-full p-2 mx-2"
+        >
+          Sign Up
+        </button>
+      </form>
+      {mailListState == MailListState.AlreadySignedUp && (
+        <>
+          <p className="m-2 mt-1 p-2 text-red-400">
+            You already signed up with that email address
+          </p>
+        </>
+      )}
+      {mailListState == MailListState.SucessfullySignedUp && (
+        <>
+          <p className="m-2 mt-1 p-2">
+            You have successfully signed up to our mailing list.
+          </p>
+        </>
+      )}
+      {mailListState == MailListState.ErrorSigningUp && (
+        <>
+          <p className="m-2 mt-1 p-2 text-red-400">
+            An error occured an you were not signed up for the mailing list.
+          </p>
+        </>
+      )}
     </div>
   );
 }
